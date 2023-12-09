ded41992f35577b35a5de0fd961662a97a1e6163
malikaziz <azizak2119@gmail.com>
2023-12-09 18:12:01 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hadiah_Teknikal"
+include(":app")
